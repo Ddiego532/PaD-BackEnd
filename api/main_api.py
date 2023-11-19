3fe70af4d86aa05cb27c2faa084b5589d34482c7
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2023-11-19 17:55:21 -0300

    all_news endpoint created
    speedrun

--- api/main_api.py
+++ api/main_api.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 import uvicorn
+import os
+import json 
+
+FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+SCRAPPED_NEWS_PATH = os.path.join(os.path.dirname(FILE_PATH), "scraper", "output_data", "all_news.json")
+
+print(SCRAPPED_NEWS_PATH)
 
 app = FastAPI()
 
 @app.get("/")
 async def welcome():
     return {"message": "Hello World"}
+
+@app.get("/all_news/")
+async def all_news():
+    try:
+        with open(SCRAPPED_NEWS_PATH, "+r", encoding="utf-8") as json_file:
+            return json.load(json_file)
+    except IOError:
+        return {"error": "all_news.json file not found!"}
