12ecee9b713f44860764a4ae21711fa234774eaf
gonzalogaticao <gonzalogaticaosorio@gmail.com>
2023-11-24 20:08:37 -0300

    try | except en doclen

--- ranking/doc_len.py
+++ ranking/doc_len.py
@@ -13,14 +13,20 @@ def main():
     path = r"indexacion/data/all_content.txt"
     output_path = r"ranking/data/doclen.txt"
 
-    with open(path, "r",encoding='utf-8') as archivo:
-        with open(output_path, "w",encoding='utf-8') as output_file:
-            for num_linea, linea in enumerate(archivo, start=1):
-                palabras = linea.lower().split()
-                document_len = sum(1 for palabra in palabras if len(check_word(palabra)) > 0)
-                
-                # Escribir la longitud de la línea en el archivo "doclen.txt"
-                output_file.write(f"{document_len}\n")
+    try: 
+        with open(path, "r",encoding='utf-8') as archivo:
+            with open(output_path, "w",encoding='utf-8') as output_file:
+                for num_linea, linea in enumerate(archivo, start=1):
+                    palabras = linea.lower().split()
+                    document_len = sum(1 for palabra in palabras if len(check_word(palabra)) > 0)
+                    
+                    # Escribir la longitud de la línea en el archivo "doclen.txt"
+                    output_file.write(f"{document_len}\n")
+
+        print("Archivo 'doclen.txt' creado")
+
+    except Exception as e:
+            print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
