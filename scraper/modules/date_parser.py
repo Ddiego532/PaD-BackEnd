49121e929826428c430dc71a82f64757ccb2760d
Gualterguaguita <guzmanmartin231@gmail.com>
2023-12-14 23:36:55 -0300

    fixed scrapper and date parser
    removed problematic source, added error handle for the date parser

--- scraper/modules/date_parser.py
+++ scraper/modules/date_parser.py
@@ -1,11 +1,14 @@
-from datetime import datetime
+from datetime import datetime, date
 import locale
 locale.setlocale(locale.LC_TIME, 'es_ES')
 
 # parsear a dd/mm/yy.
 def parse_date(raw_date : str, parsing_form : str):
-    # Parsear la fecha original
-    date_parsed = datetime.strptime(raw_date, parsing_form)
+    try:
+        # Parsear la fecha original
+        date_parsed = datetime.strptime(raw_date, parsing_form)
 
-    # Formatear la nueva fecha en el formato deseado
-    return date_parsed.strftime("%d/%m/%Y")
+        # Formatear la nueva fecha en el formato deseado
+        return date_parsed.strftime("%d/%m/%Y")
+    except ValueError as e:
+        return date.today().strftime("%d/%m/%Y")
