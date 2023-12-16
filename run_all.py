614b7a77cd9ddf4e32be96563b61d46ab1a93ebb
Gualterguaguita <guzmanmartin231@gmail.com>
2023-12-16 14:40:23 -0300

    run all file
    :)

---
+++ run_all.py
@@ -0,0 +1,21 @@
+import os
+
+#Archivo encargado de ejecutar todo de forma secuencial, scrapeo, indexado y ranking.
+
+def ejecutar_archivo(ruta):
+    try:
+        os.system(f"python {ruta}")
+    except Exception as e:
+        print(f"Error al ejecutar {ruta}: {str(e)}")
+
+if __name__ == "__main__":
+    carpetas_y_archivos = [
+        "scraper/main_scraper.py",
+        "indexacion/json_to_txt.py",
+        "indexacion/mapreduce.py",
+        "ranking/doc_len.py"
+    ]
+
+    for archivo in carpetas_y_archivos:
+        ruta_completa = os.path.join(os.path.dirname(__file__), archivo)
+        ejecutar_archivo(ruta_completa)
